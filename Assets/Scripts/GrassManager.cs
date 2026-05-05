b2d45e75796937c2bb096bd8c68da6edae57be4e
natsure_mac <napnature8436@gmail.com>
2026-05-05 21:14:08 +0900

    fix:GrassManagerに壁の生成とカメラの位置調整実装

--- Assets/Scripts/GrassManager.cs
+++ Assets/Scripts/GrassManager.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class GrassManager : MonoBehaviour
 {
     [SerializeField] Transform grassParent;
+    [SerializeField] GameObject grassCubePrefab; // 1x1
+    [SerializeField] Transform wallParent;
+    [SerializeField] GameObject wallPrefab; // 透明
     [SerializeField] Color grassColor;
     [SerializeField] Color soilColor;
-    [SerializeField] GameObject grassCubePrefab; // 1x1
     [SerializeField] int ranchSize = 11; // 正方形
+    float wallHeight = 5f;
     float time = 0;
     [SerializeField] float growthTime = 120f;
     private List<Material> cubeMList = new List<Material>();
@@ -18,17 +21,11 @@ public class GrassManager : MonoBehaviour
 
     void Start()
     {
-        for (int x = 0; x < ranchSize; x++)
-        {
-            for (int z = 0; z < ranchSize; z++)
-            {
-                GameObject cube = Instantiate(grassCubePrefab);
-                cube.transform.parent = grassParent;
-                cube.transform.position = new Vector3(x, -0.5f, z);
-                cube.transform.localScale = new Vector3(1, 1, 1);
-                cubeMList.Add(cube.GetComponent<Material>());
-            }
-        }
+        GenerateGrass();
+        GenerateWall();
+        // 10x10で30度見下ろすとy10,z15で距離が5_/13くらい
+        // y = ranchSize, z = ranchSize + 1.5f でいいか
+        CameraPositioning();
     }
 
     void Update()
@@ -58,5 +55,48 @@ public class GrassManager : MonoBehaviour
     int GetID(int x, int z)
     {
         return x * ranchSize + z;
+    }
+
+    void GenerateGrass()
+    {
+        for (int x = 0; x < ranchSize; x++)
+        {
+            for (int z = 0; z < ranchSize; z++)
+            {
+                GameObject cube = Instantiate(grassCubePrefab);
+                cube.name = $"GrassCube_{GetID(x, z)}";
+                cube.transform.parent = grassParent;
+                cube.transform.position = new Vector3(x, -0.5f, z);
+                cube.transform.localScale = new Vector3(1, 1, 1);
+                cubeMList.Add(cube.GetComponent<Material>());
+            }
+        }
+    }
+
+    // 草原を囲むcolliderの壁を四方に配置
+    void GenerateWall()
+    {
+        float half = (ranchSize - 1) / 2f;
+        GenerateWallPosition(half, -1, 0); // ↓
+        GenerateWallPosition(-1, half, 90); // ←
+        GenerateWallPosition(half, ranchSize, 0); // ↑
+        GenerateWallPosition(ranchSize, half, 90); // →
+    }
+
+    void GenerateWallPosition(float x, float z, float yRot)
+    {
+        GameObject wall = Instantiate(wallPrefab);
+        wall.transform.parent = wallParent;
+        wall.transform.localScale = new Vector3(ranchSize, wallHeight, 1);
+        wall.transform.position = new Vector3(x, wallHeight / 2f, z);
+        wall.transform.rotation = Quaternion.Euler(0, yRot, 0);
+    }
+
+    // 動かさないなら本来絶対にスクリプトからやるべきではない
+    // ステージの生成もeditor完結にするべき
+    void CameraPositioning()
+    {
+        Camera.main.transform.position = new Vector3((ranchSize - 1) / 2f, ranchSize, -ranchSize * 1.5f);
+        Camera.main.transform.rotation = Quaternion.Euler(30, 0, 0);
     }
 }
